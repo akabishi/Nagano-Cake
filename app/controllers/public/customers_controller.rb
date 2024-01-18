c14f3dea1b4cd20506d9083a16fd81ffe2595d20
ymakki <147698184+ymakki@users.noreply.github.com>
2024-01-18 16:18:00 +0000

    Make-Customer

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,23 @@
 class Public::CustomersController < ApplicationController
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def edit
+    @customer = Customer.find(params[:customer_id])
+  end
+
+  def update
+    @customer=Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to customer_path(current_customer)
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :phone_number)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,22 @@
 Rails.application.routes.draw do
 
   # 顧客用
-   #URL /customers/sign_in ...
+  #URL /customers/sign_in ...
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
 
-  # 管理者用
-   #URL /admin/sign_in ...
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-    sessions: "admin/sessions"
-  }
-
-
   # 会員側のルーティング設定
   scope module: :public do
-
     # トップページ
-    root :to =>"homes#top"
+    root to: "homes#top"
     # アバウトページ
     get "home/about"=>"homes#about"
     # 商品
    resources :items, only: [:index, :show]
     # 顧客
-    resources :customers, only: [:new, :index, :create, :show] do
+    resources :customers do
       get 'my_page' => 'customers#show'
       get 'information/edit' => 'customers#edit'
       patch 'information' => 'customers#update'
@@ -42,11 +34,18 @@ Rails.application.routes.draw do
     end
     # 配送先
     resources :addresses, only: [:index, :edit, :create, :update, :destroy]
-
   end
+
+  # 管理者用
+  #URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
 
   # 管理者側のルーティング設定
   namespace :admin do
+    # トップページ
+    root to: "homes#top"
     # 商品
     resources :items, only: [:new, :index, :create, :show, :edit, :update]
     # ジャンル
@@ -58,8 +57,6 @@ Rails.application.routes.draw do
       # 製作ステータスの更新
       resources :order_details, only: [:update]
     end
-
   end
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
