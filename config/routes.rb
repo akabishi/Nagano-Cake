44b89ad78173840852e2e703631443172fe6ac84
EC2 Default User <0311.hnmyuya@gmail.com>
2024-01-19 04:59:50 +0000

    途中

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
-  }
+}
 
   # 管理者用
    #URL /admin/sign_in ...
@@ -13,7 +13,6 @@ Rails.application.routes.draw do
 devise_for :admin, skip: [:registrations, :passwords], controllers: {
   sessions: 'admin/sessions'
 }
-
 
   # 会員側のルーティング設定
   scope module: :public do
