260cfcf435d39fcc430f28e5d9573f1a0235ea1f
EC2 Default User <0311.hnmyuya@gmail.com>
2024-01-22 05:47:09 +0000

    ファイルを作成

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -3,6 +3,17 @@ class Admin::SearchController < ApplicationController
     @query = params[:query]
     @items, @customers = perform_search(@query)
   end
+  
+  def search
+    @word = params[:word]
+    @search = params[:search]
+    @range = params[:range]
+
+    if @range == "商品"
+      @items = Item.looks(@search, @word)
+      @genres = Genre.all
+    end
+  end
 
   private
 
