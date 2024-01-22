ddd0205efb923da45d9dea5f680d17638d16c8f3
ymakki <147698184+ymakki@users.noreply.github.com>
2024-01-22 09:07:31 +0000

    Add admin show.view

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,12 @@ class Order < ApplicationRecord
   # enum の定義
   enum payment_method: { credit_card: 0, transfer: 1 }
 
+  enum status: {
+    awaiting_payment: 0,
+    payment_confirmed: 1,
+    in_production: 2,
+    preparing_for_shipment: 3,
+    shipped: 4
+  }
+
 end
