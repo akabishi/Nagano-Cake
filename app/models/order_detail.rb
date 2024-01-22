5bbe8efb6d74b5aa6c52f93dc3e72583d1502674
ChisatoKano <129806087+kano0000@users.noreply.github.com>
2024-01-22 03:44:17 +0000

    Add MakingStatus

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,13 @@
 class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :item
+  
+  # 制作ステータス => 0:製作不可 1:製作待ち 2:製作中 3:製作完了
+  enum making_status: {
+    unable_making: 0,
+    waiting_making: 1,
+    in_making: 2,
+    finish_making: 3
+  }
+ 
 end
