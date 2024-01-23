360907594ecdec2a2ff76aa8a1888fde4cff4bce
EC2 Default User <0311.hnmyuya@gmail.com>
2024-01-23 04:48:32 +0000

    ヘッダーレイアウト

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -4,4 +4,20 @@ class Public::SearchesController < ApplicationController
     @genre = Genre.find(params[:id])
     @items = Item.where(genre_id: @genre.id).page(params[:page]).per(8)
   end
+  
+  def search
+    @word = params[:word]
+    @search = params[:search]
+    @range = params[:range]
+
+    if @range == "商品"
+      @items = Item.looks(@search, @word)
+      @genres = Genre.all
+
+      # HTMLフォーマットのリクエストに対するテンプレートを返す
+      respond_to do |format|
+        format.html
+      end
+    end
+  end
 end
